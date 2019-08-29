7546848bb9b88d6ab7b6e98b0876277c0a58ea06
brosie224 <barosengarten@gmail.com>
2019-08-29 12:29:20 -0400

    Added a downvote button

--- src/components/CharacterQuote.jsx
+++ src/components/CharacterQuote.jsx
@@ -12,6 +12,14 @@ class NewQuote extends Component {
     });
   };
 
+  handleDownVote = () => {
+    if (this.state.votes > 0) {
+      this.setState({
+        votes: this.state.votes - 1
+      });
+    }
+  };
+
   render() {
     const { quote } = this.props;
     return (
@@ -19,6 +27,9 @@ class NewQuote extends Component {
         {quote.content}{" "}
         <button className="vote" onClick={this.handleUpVote}>
           +
+        </button>{" "}
+        <button className="vote" onClick={this.handleDownVote}>
+          -
         </button>{" "}
         {this.state.votes}
       </Container>
